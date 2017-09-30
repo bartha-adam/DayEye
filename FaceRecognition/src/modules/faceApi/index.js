bd24512a97a0b70bef3eb19f1643d0bad17c8acf
Unknown <electronicplayground051@gmail.com>
2017-10-01 01:39:18 +0300

    fdasfdsa

--- FaceRecognition/src/modules/faceApi/index.js
+++ FaceRecognition/src/modules/faceApi/index.js
@@ -140,7 +140,7 @@ let faceApi = function (config) {
       cmd: "stop_detection"
     }))
 
-    delay(500);
+    sleep(500);
 
     mjpgStreamerProcess = spawn("mjpg_streamer", ["-i", "input_raspicam.so"]);
 
@@ -162,8 +162,8 @@ let faceApi = function (config) {
     if (mjpgStreamerProcess) {
       mjpgStreamerProcess.kill();
 
-      delay(500);
-      
+      sleep(500);
+
       client.publish( "dayeye/face/in",JSON.stringify({
         cmd: "start_detection"
       }))
